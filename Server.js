497c681745b18ddad8c24c78266e256aaa6d1581
tejassgg <tejassgg@gmail.com>
2023-05-08 22:40:40 +0530

    Adding Axios to Package.json

--- Server.js
+++ Server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/users", userRoutes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const MONGOOSE_URL = "mongodb+srv://tejassgg:june2014@cluster0.lsryd.mongodb.net/DILDB?retryWrites=true&w=majority"
 
 mongoose.connect(MONGOOSE_URL, {useNewUrlParser: true})
